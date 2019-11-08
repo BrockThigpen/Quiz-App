fdce88312937712cdff0ce725da617f31ed558e3
Brock T <brockthigpen@yahoo.com>
2019-11-08 12:23:23 -0500

    added logic start button logic and logic to show questions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,15 +1,38 @@
 // DOM elements
-let highscoreBtn = document.getElementById('#highscoreBtn');
-let timerEl = document.getElementById('#timer');
-var startH = document.getElementById('#startHead');
-var startP = document.getElementById('startP');
-var startBtn = document.getElementById('#startBtn');
-let questionEl = document.getElementById('#theQuestion');
-let answersEl = document.getElementById('#answers');
+const highscoreBtn = document.getElementById('highscoreBtn');
+const timerEl = document.getElementById('timer');
+const startH = document.getElementById('startHead');
+const startP = document.getElementById('startP');
+const startBtn = document.getElementById('startBtn');
+const questionEl = document.getElementById('theQuestion');
+const answersEl = document.getElementById('answers');
+
+let questionsShuflle, questionIndex;
 
 // startBtn on click
-startBtn.addEventListener("click", () => {
+startBtn.addEventListener("click", startQuiz);
+
+function startQuiz(){
     startBtn.classList.add('hide');
     startH.classList.add('hide');
-    startp.classList.add('hide');
-});
+    startP.classList.add('hide');
+    questionEl.classList.remove('hide');
+    answersEl.classList.remove('hide');
+    questionsShuflle = questions.sort(() => Math.random() - .5 );
+    questionIndex = 0;
+    showNextQ();
+}
+
+// render quetions
+function showNextQ(){
+    rendorQuestion(questionsShuflle[questionIndex])
+}
+function rendorQuestion(questions) {
+    questionEl.innerHTML = questions.title;
+    questions.choices.forEach(choices =>{
+        const holder = document.createElement('li');
+        holder.innerText = choices.text;
+        answersEl.appendChild(holder)
+
+    })
+}
