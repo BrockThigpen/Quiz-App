47ff1ecaecb526b856226ceabea5aa224d439829
Brock T <brockthigpen@yahoo.com>
2019-11-08 16:36:14 -0500

    added timer and logic for answering questions wrong

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,11 +7,13 @@ const startBtn = document.getElementById('startBtn');
 const questionEl = document.getElementById('theQuestion');
 const answersEl = document.getElementById('answers');
 
+// variables
 let questionsShuflle, questionIndex;
+let score = 0;
+let timeRemaing = 0;
 
 // startBtn on click
 startBtn.addEventListener("click", startQuiz);
-
 function startQuiz(){
     startBtn.classList.add('hide');
     startH.classList.add('hide');
@@ -23,23 +25,42 @@ function startQuiz(){
     showNextQ();
     setTimer();
 }
-
-// render quetions
+// render quetions and choices 
 function showNextQ(){
     rendorQuestion(questionsShuflle[questionIndex])
 }
 function rendorQuestion(questions) {
     questionEl.innerHTML = questions.title;
-    for(var i = 0; i < questions.choices.length; i++){
+    for(let i = 0; i < questions.choices.length; i++){
         const holder = document.createElement('li');
         holder.innerText = questions.choices[i];
-        holder.setAttribute('data-index', i);
         answersEl.appendChild(holder)
     }
 }
-
-
-
+// answer on click
+answersEl.addEventListener('click', function (e){
+    let target = event.target;
+    if(target.matches('li') === true){
+        let picked = target.innerText;
+        if(picked === questionsShuflle[questionIndex].answer){
+            score = score + 20;
+            showNextQ();
+            console.log(score);
+        }else{
+            timeRemaing = timeRemaing - 10;
+        }
+    }
+})
+// timer
 function setTimer () {
-
+    timerEl.innerText = 'Time: 75';
+    timeRemaing = questions.length * 15;
+    let timer = setInterval( () => {
+        timeRemaing--;
+        timerEl.innerText = 'Time: '+ timeRemaing;
+        if(timeRemaing <= 0){
+            clearInterval(timer);
+            // end attempt, get score, send to scores page
+        }
+    }, 1000);
 }
