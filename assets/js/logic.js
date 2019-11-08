2b4b2550e1bde55b784d38428de779f5c80c340f
Brock T <brockthigpen@yahoo.com>
2019-11-08 10:00:17 -0500

    added dom elements to logic.js

---
+++ assets/js/logic.js
@@ -0,0 +1,8 @@
+// DOM elements
+let highscoreBtn = document.getElementById('#highscoreBtn');
+let timerEl = document.getElementById('#timer');
+let startH = document.getElementById('#startHead');
+let startP = document.getElementById('startP');
+let startBtn = document.getElementById('#startBtn');
+let questionEl = document.getElementById('#theQuestion');
+let answersEl = document.getElementById('#answers');
